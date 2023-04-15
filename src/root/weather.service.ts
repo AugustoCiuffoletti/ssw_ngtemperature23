d05c2bd37f1c16661a344c5c763fa17998cb5e65
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-04-15 17:54:49 +0200

    1 file modified

--- src/root/weather.service.ts
+++ src/root/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
-import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 
 @Injectable({
   providedIn: 'root'
