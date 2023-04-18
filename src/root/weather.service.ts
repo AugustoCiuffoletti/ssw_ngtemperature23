baedc3360075c8171b019e1129c88de36a05cddf
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-04-18 14:50:44 +0200

    1 file modified

--- src/root/weather.service.ts
+++ src/root/weather.service.ts
@@ -5,16 +5,13 @@ import { ajax, AjaxResponse } from 'rxjs/ajax';
 @Injectable({
   providedIn: 'root'
 })
-
 export class WeatherService {
   apiKey: string = "d0475be3a1967b1b49dfc02c8128001a";
   URL: string  =
     "https://api.openweathermap.org/data/2.5/weather?APPID=" +
     this.apiKey +
     "&units=metric&q=";
-
   constructor() {}
-
   public getData(CityName: string): Observable<AjaxResponse<any>> {
     return ajax({
       method: 'GET',
